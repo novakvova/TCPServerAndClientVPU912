b7f8b0d8769522dab2e51401549264fda8115efd
novakvova <novakvova@gmail.com>
2021-05-23 16:40:12 +0300

    Begin create chat

---
+++ WpfClient/UserControlMessageSent.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfClient
+{
+    /// <summary>
+    /// Interaction logic for UserControlMessageSent.xaml
+    /// </summary>
+    public partial class UserControlMessageSent : UserControl
+    {
+        public UserControlMessageSent()
+        {
+            InitializeComponent();
+        }
+    }
+}
